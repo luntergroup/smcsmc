852a35ea6721ee6709ee5d6c82cf6a9abca7bffe
Joe Zhu <joe.zhu@well.ox.ac.uk>
2014-05-28 14:11:43 +0000

    commit changes in singple particle many repeats
    git-svn-id: svn+ssh://florence/Net/fs1/groups/bioinformatics/svn/repos/branches/optimising_count@2677 09689bc1-c481-4e07-9956-135ddad18f52

--- experiment2_many_repeats/single_particle_many_repeats.sh
+++ experiment2_many_repeats/single_particle_many_repeats.sh
@@ -2,7 +2,7 @@
 rm recomb_rate_no_prune Ne_no_prune
 for rep in $(seq 1 1 10000)
 	do 
-	pf-ARG -Np 1 -o trial${rep} -seed ${rep}
+	pf-ARG -Np 1 -o trial_no_prune${rep} -seed ${rep}
 	grep "inferred recomb rate = " trial_no_prune${rep}.log | sed -e "s/inferred recomb rate =//" >> recomb_rate_no_prune
 	tail -1 trial_no_prune${rep}.log | sed -e "s/  (       0 ) |  //" >> Ne_no_prune
 	rm trial_no_prune${rep}*
