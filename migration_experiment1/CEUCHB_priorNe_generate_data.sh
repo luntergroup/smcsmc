b4958429206c9eb453b86dc3f521604f0dff2dbe
Joe Zhu <joe.zhu@well.ox.ac.uk>
2014-07-08 22:30:05 +0000

    update CEU CHB population structure
    git-svn-id: svn+ssh://florence/Net/fs1/groups/bioinformatics/svn/repos/branches/optimising_count@2866 09689bc1-c481-4e07-9956-135ddad18f52

---
+++ migration_experiment1/CEUCHB_priorNe_generate_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source CEUCHB_priorNe_param.src
+
+mkdir ${dir}
+positionfile=${fileprefix}position
+segfile=${fileprefix}seg
+
+scrm ${nsam} 1 ${cmd} -l 100000 -seed 1 > ${fileprefix}
+
+grep 'positions' ${fileprefix} | sed -e 's/positions: //' >  ${positionfile}
+tail -${nsam} ${fileprefix} > ${segfile}
+
+echo "./ms2something.py vcf ${seqlen} ${positionfile} ${segfile} ${fileprefix}"
+./ms2something.py vcf ${seqlen} ${positionfile} ${segfile} ${fileprefix}
+ 
