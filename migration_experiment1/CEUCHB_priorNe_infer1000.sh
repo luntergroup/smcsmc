dc68229d4238f612cb96bec0c8737543a1f38ab6
Joe Zhu <joe.zhu@well.ox.ac.uk>
2014-07-09 02:05:51 +0000

    update run scripts
    git-svn-id: svn+ssh://florence/Net/fs1/groups/bioinformatics/svn/repos/branches/optimising_count@2870 09689bc1-c481-4e07-9956-135ddad18f52

--- migration_experiment1/CEUCHB_priorNe_infer1000.sh
+++ migration_experiment1/CEUCHB_priorNe_infer1000.sh
@@ -27,9 +27,10 @@ pattern="1*3+25*2+1*4+1*6"
 tmax=8
 
 infer_mig_pattern="-eM 0.02599 1 -eM 0.03736 1 -eM 0.05044 1 "
-for TASK in 'seq $SGE_TASK_START $SGE_TASK_END';
-    do 
-    outprefix=${prefix}rep${TASK}
-    pf-ARG -l 0 ${cmd} ${split} ${infer_mig_pattern} ${pop_struct} -EM ${EM} -Np ${Np} -o ${outprefix} -vcf ${data} -seed ${TASK}
-    done
+#for TASK in 'seq $SGE_TASK_START $SGE_TASK_END';
+    #do 
+TASK=$SGE_TASK_ID
+outprefix=${prefix}rep${TASK}
+pf-ARG -l 0 ${cmd} ${split} ${infer_mig_pattern} ${pop_struct} -EM ${EM} -Np ${Np} -o ${outprefix} -vcf ${data} -seed ${TASK}
+    #done
 # should add pattern to this!
