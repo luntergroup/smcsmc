bc2b2b89f432908295a24397a169db89c0a966bd
Joe Zhu <joe.zhu@well.ox.ac.uk>
2014-09-26 18:47:42 +0000

    correct directory of 4sample_smaller_grid
    git-svn-id: svn+ssh://florence/Net/fs1/groups/bioinformatics/svn/repos/branches/optimising_count@3159 09689bc1-c481-4e07-9956-135ddad18f52

--- particle_vs_seqlen_4sample_small_grid_presents/simulation_runs/copy_data.sh
+++ particle_vs_seqlen_4sample_small_grid_presents/simulation_runs/copy_data.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 rm Particle*
-scp joezhu@login1-cluster3.well.ox.ac.uk:/well/gerton/joezhu/particle_vs_seqlen_4sample/* .
+scp joezhu@login1-cluster3.well.ox.ac.uk:/well/gerton/joezhu/particle_vs_seqlen_4sample_smaller_grid/* .
 
