4ad978954758a3fc5d6033ec5a4f8e0d458886ba
shajoezhu <sha.joe.zhu@gmail.com>
2014-12-02 18:46:08 +0000

    include more test runs

--- tests/test_binaries.sh
+++ tests/test_binaries.sh
@@ -2,25 +2,25 @@
 
 function test_smcsmc {
   echo -n " smcsmc $@ "
-  for i in `seq 7 10`; do
+  for i in `seq 1 20`; do
     echo -n "."
 
-    ## Test using smcsmc self-checks
-    #./smcsmc_dbg $@ -seed $i > /dev/null 
-    #if [ $? -ne 0 ]; then
-      #echo ""
-      #echo "Executing \"./smcsmc_dbg $@ -seed $i\" failed."
-      #echo "Debug Call: make -mj2 smcsmc_dbg && ./smcsmc_dbg $@ -seed $i 2>&1 | less"
-      #exit 1
-    #fi
+    # Test using smcsmc self-checks
+    ./smcsmc_dbg $@ -seed $i > /dev/null 
+    if [ $? -ne 0 ]; then
+      echo ""
+      echo "Executing \"./smcsmc_dbg $@ -seed $i\" failed."
+      echo "Debug Call: make -mj2 smcsmc_dbg && ./smcsmc_dbg $@ -seed $i 2>&1 | less"
+      exit 1
+    fi
 
-    ## Test for memory leaks
-    #valgrind --error-exitcode=1 --leak-check=full -q ./smcsmc $@ -seed $i > /dev/null
-    #if [ $? -ne 0 ]; then
-      #echo ""
-      #echo "Valgrind check of \"./smcsmc $@ -seed $i\" failed."
-      #exit 1
-    #fi
+    # Test for memory leaks
+    valgrind --error-exitcode=1 --leak-check=full -q ./smcsmc $@ -seed $i > /dev/null
+    if [ $? -ne 0 ]; then
+      echo ""
+      echo "Valgrind check of \"./smcsmc $@ -seed $i\" failed."
+      exit 1
+    fi
 
 	# Test for lagging utility
 	./smcsmc $@ -seed $i -o tmp > /dev/null
@@ -38,15 +38,15 @@ function test_smcsmc {
 }
 
 echo "Testing (Decide the case name later ... ) "
- #test_smcsmc -nsam 2 -Np 1000 -r 130 1000000 || exit 1
- #test_smcsmc -nsam 3 -Np 1000 -r 130 1000000 || exit 1
- #test_smcsmc -nsam 4 -Np 1000 -r 130 1000000 || exit 1
- #test_smcsmc -nsam 5 -Np 1000 -r 130 1000000 || exit 1
+ test_smcsmc -nsam 2 -Np 1000 -r 130 1000000 || exit 1
+ test_smcsmc -nsam 3 -Np 1000 -r 130 1000000 || exit 1
+ test_smcsmc -nsam 4 -Np 1000 -r 130 1000000 || exit 1
+ test_smcsmc -nsam 5 -Np 1000 -r 130 1000000 || exit 1
 echo ""
 
 echo "Testing -EM "
- #test_smcsmc -nsam 2 -Np 1000 -r 130 1000000 -EM 2 || exit 1
- #test_smcsmc -nsam 3 -Np 1000 -r 130 1000000 -EM 2 || exit 1
- test_smcsmc -nsam 4 -Np 1000 -r 130 1000000 -EM 1 || exit 1
- #test_smcsmc -nsam 5 -Np 1000 -r 130 1000000 -EM 2 || exit 1
+ test_smcsmc -nsam 2 -Np 1000 -r 130 1000000 -EM 2 || exit 1
+ test_smcsmc -nsam 3 -Np 1000 -r 130 1000000 -EM 2 || exit 1
+ test_smcsmc -nsam 4 -Np 1000 -r 130 1000000 -EM 2 || exit 1
+ test_smcsmc -nsam 5 -Np 1000 -r 130 1000000 -EM 2 || exit 1
 echo ""
