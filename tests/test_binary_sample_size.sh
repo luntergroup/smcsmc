1ea1596046799458a5bcbc0b3364b0ce149c0994
Joe Zhu <sha.joe.zhu@gmail.com>
2014-12-03 15:12:35 +0000

    check upto 22 samples

--- tests/test_binary_sample_size.sh
+++ tests/test_binary_sample_size.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Timeout at 24 individuals
+#smcsmc -nsam 23 -Np 1000 -r 130 1000000 . done.
+#smcsmc -nsam 24 -Np 1000 -r 130 1000000 .command cd tests; ./test_binary_sample_size.sh took more than 3 minutes since last output
 
 source test_binaries.src
 
@@ -6,13 +9,13 @@ begin=1
 end=1
 
 echo "Testing (Decide the case name later ... ) "
-for size in $(seq 2 30);do 
+for size in $(seq 2 22);do 
  test_smcsmc -nsam ${size} -Np 1000 -r 130 1000000 || exit 1
 done
 echo ""
 
 echo "Testing -EM "
-for size in $(seq 2 30);do 
+for size in $(seq 2 22);do 
  test_smcsmc -nsam ${size} -Np 1000 -r 130 1000000 -EM 2 || exit 1
 done
 echo ""
