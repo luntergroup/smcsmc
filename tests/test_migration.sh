7b4c341a5c45959784c7e69e7617d7e9d3f05859
Gerton Lunter <gerton.lunter@gmail.com>
2015-03-10 16:46:47 +0000

    added simple regression test for migration; nicer debug output

---
+++ tests/test_migration.sh
@@ -0,0 +1,17 @@
+echo "Migration test"
+echo
+
+echo "Building test data..."
+python pop_struct.py sim-1-migration 2 1 median
+echo
+
+cmd="../smcsmc -nsam 2 -t 1000 -r 200 1000000 -I 2 1 1 -ej 0.6 1 2 -eM 0 0.1 -EM 0 -Np 1000 -seg sim-1-migrationSamples2msdata1.seg -seed 1 1 1 -p 1*3+15*4+1 -tmax 4"
+echo "Running migration test..."
+echo "Command line:"
+echo ${cmd}
+${cmd} | sed 's/..Particle[^\n]*completed.//g' > test_migration.output
+diff test_migration.output test_migration.truth
+
+
+
+
