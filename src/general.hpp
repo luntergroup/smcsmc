0177361bff70902a2ab2c7449c3760db89fc7be9
Gerton Lunter <gerton.lunter@gmail.com>
2015-07-02 15:57:12 +0100

    forgot to add header file

---
+++ src/general.hpp
@@ -0,0 +1,10 @@
+#ifndef __general__
+#define __general__
+
+
+// to explictly mark a variable as unused, and avoid 'variable not used' warnings (SO 777261)
+#define _unused(x) ((void)x)
+
+
+#endif // __general__
+
