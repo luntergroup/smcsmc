deb543a891efdde7f76e68a324e21c247462d74a
Joe Zhu <sha.joe.zhu@gmail.com>
2015-10-10 16:49:14 +0100

    update travis scripts

--- tests/test_binary_sample_size.sh
+++ tests/test_binary_sample_size.sh
@@ -8,14 +8,14 @@ source test_binaries.src
 begin=1
 end=1
 
-echo "Testing (Decide the case name later ... ) "
-for size in $(seq 2 22);do 
+echo "Testing the program for different number of taxa  "
+for size in $(seq 2 8);do
  test_smcsmc -nsam ${size} -Np 1000 -r 130 1000000 || exit 1
 done
 echo ""
 
 echo "Testing -EM "
-for size in $(seq 2 6);do 
+for size in $(seq 2 6);do
  test_smcsmc -nsam ${size} -Np 1000 -r 130 1000000 -EM 2 || exit 1
 done
 echo ""
