e56e9001cfc15244beb4af09dfa5b0335c163db4
Joe Zhu <sha.joe.zhu@gmail.com>
2015-10-12 18:24:42 +0100

    update travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,15 +4,14 @@ echo $LC_ALL
 if [ $TRAVIS_OS_NAME == linux ]; then
     sudo apt-get update -qq
     sudo apt-get install -qq git-core libcppunit-dev graphviz valgrind r-base-core
+fi
 
-    if [ $CXX == g++ ]; then
-        #sudo apt-get install -qq g++-4.8
-        export CC="gcc-4.8";
-        export CXX="g++-4.8";
-        export LINK="gcc-4.8";
-        export LINKXX="g++-4.8";
-    fi
-
+if [ $CXX == g++ ]; then
+    #sudo apt-get install -qq g++-4.8
+    export CC="gcc-4.8";
+    export CXX="g++-4.8";
+    export LINK="gcc-4.8";
+    export LINKXX="g++-4.8";
 fi
 
 if [ $TRAVIS_OS_NAME == osx ]; then
