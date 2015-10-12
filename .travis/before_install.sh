1770661ab765e116167091312b691f0572e0674c
Joe Zhu <sha.joe.zhu@gmail.com>
2015-10-12 20:58:12 +0100

    update travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo $LANG
 echo $LC_ALL
-if [ $TRAVIS_OS_NAME == linux ]; then
+if [ $TRAVIS_OS_NAME = linux ]; then
+    echo "Linux"
     sudo apt-get update -qq
     sudo apt-get install -qq git-core libcppunit-dev graphviz valgrind r-base-core
 fi
 
-if [ $CXX == g++ ]; then
+if [ $CXX = "g++" ]; then
+    echo "change g++"
     #sudo apt-get install -qq g++-4.8
     export CC="gcc-4.8";
     export CXX="g++-4.8";
@@ -14,6 +16,7 @@ if [ $CXX == g++ ]; then
     export LINKXX="g++-4.8";
 fi
 
-if [ $TRAVIS_OS_NAME == osx ]; then
+if [ $TRAVIS_OS_NAME = osx ]; then
+    echo "OSX"
     brew update && brew install llvm cppunit graphviz valgrind && brew link --force llvm
 fi
