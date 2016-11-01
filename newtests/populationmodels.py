81986368e62d1120a16422bc66025768c49a9597
Gerton Lunter <gerton.lunter@gmail.com>
2016-11-01 20:36:31 +0000

    better python2.7/3 support; fixed path bug

--- newtests/populationmodels.py
+++ newtests/populationmodels.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import sys
 import subprocess
@@ -83,7 +84,7 @@ class Population:
             filename = self.filename
 
         # complete the command
-        command = "{cmd} {args} -seed {seed}".format(cmd = "scrm",
+        command = "{cmd} {args} -seed {seed}".format(cmd = self.scrmpath,
                                                      args = command,
                                                      seed = ' '.join(map(str,self.seed)))
 
