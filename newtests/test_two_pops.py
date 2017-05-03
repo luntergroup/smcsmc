06703277f1c9a7ece4919a9be98dc4f678ffdd04
DonnaHenderson <dch0627@gmail.com>
2017-05-03 14:16:51 +0100

    set targets for test_two_pops.py based on 2b3a; have not altered ess or found a way to skip dummy tests

--- newtests/test_two_pops.py
+++ newtests/test_two_pops.py
@@ -27,7 +27,7 @@ class TestTwoPops(TestGeneric):
 
         self.lag = 1
         self.popt = None
-        self.em = 0
+        self.em = 4
         self.np = 100
         self.seed = (8,)
         self.bias_heights = None
@@ -52,8 +52,7 @@ class TestTwoPopsSplit(TestTwoPops):
         self.pop.split_command = "-ej .5 2 1"
 
 # This is an important regression test,
-# TO DO: calculate targets based on earlier stable verions 
-#        old targets used 34d52e63b6e7, lets update to a59a96d439c52e as it is less likely to hang in calculate_median_survival_distance
+# Note: targets were calculated based on 100 runs of 2b3a_wo_apply_immediately_hack 
 class TestTwoPopsSplitUniDirMigr(TestTwoPopsSplit):
 
     def setUp(self, fn="testdata/twopopssplit_unidirmigr"):
@@ -76,25 +75,44 @@ class TestTwoPopsSplitUniDirMigr(TestTwoPopsSplit):
 
         scaling = 1.0 / (4.0 * self.pop.N0)
 
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':0,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':1,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':2,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':0,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':1,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':1,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':2,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':2,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
+        self.targets = [{'type':"Coal", 'pop':0, 'epoch':0,
+                         'min':8209, 'max':9457, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':0,
+                         'min':8612,  'max':10077,'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':0, 'epoch':1,
+                         'min':10333,  'max':10878,  'truth': 10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':1,
+                         'min':9944,  'max':10504,  'truth': 10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':0, 'epoch':2,
+                         'min':9374,  'max':9584, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':2,
+                         'min':10000,  'max':10000, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Recomb",
+                         'min':9.91e-9, 'max':1.016e-8, 'truth':1e-8, 'ess':self.np-1},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':1.40e-6},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':5.32e-7},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':2.64e-6, 'max':3.98e-6},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':2.29e-6, 'max':3.47e-6},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':0},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':5e-6, 'max':5e-6} ]
 
         for t in self.targets:
             if t['type'] == "Migr":
@@ -102,8 +120,7 @@ class TestTwoPopsSplitUniDirMigr(TestTwoPopsSplit):
 
 
 # This is an important regression test, 
-# TO DO: calculate targets based on earlier stable verions 
-#        old targets used 34d52e63b6e7, lets update to a59a96d439c52e as it is less likely to hang in calculate_median_survival_distance
+# Note: targets were calculated based on 100 runs of 2b3a_wo_apply_immediately_hack 
 class TestTwoPopsSplitUniDirMigrInRecentEpoch(TestTwoPopsSplit):
 
     def setUp(self, fn="testdata/twopopssplit_unidirmigrinrecentepoch"):
@@ -125,33 +142,51 @@ class TestTwoPopsSplitUniDirMigrInRecentEpoch(TestTwoPopsSplit):
 
         scaling = 1.0 / (4.0 * self.pop.N0)
 
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':0,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':1,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':2,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':0,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':1,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':1,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':2,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':2,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
+        self.targets = [{'type':"Coal", 'pop':0, 'epoch':0,
+                         'min':8539, 'max':9749, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':0,
+                         'min':8453,  'max':9765,'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':0, 'epoch':1,
+                         'min':9710,  'max':10165,  'truth': 10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':1,
+                         'min':9427,  'max':9827,  'truth': 10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':0, 'epoch':2,
+                         'min':9548,  'max':9755, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':2,
+                         'min':10000,  'max':10000, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Recomb",
+                         'min':9.87e-9, 'max':1.015e-8, 'truth':1e-8, 'ess':self.np-1},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':1.10e-6},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':3.33e-7},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':2.02e-6, 'max':2.98e-6},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':1.72e-6, 'max':2.77e-6},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':0},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':5e-6, 'max':5e-6} ]
 
         for t in self.targets:
             if t['type'] == "Migr":
                 t['truth'] = scaling * self.pop.migration_rates[t['epoch']][t['from_pop']][t['to_pop']]
 
 # This is an important regression test, 
-# TO DO: calculate targets based on earlier stable verions 
-#        old targets used 34d52e63b6e7, lets update to a59a96d439c52e as it is less likely to hang in calculate_median_survival_distance                
+# Note: targets were calculated based on 100 runs of 2b3a_wo_apply_immediately_hack                
 class TestTwoPopsSplitUniDirMigrInMidEpoch(TestTwoPopsSplit):
 
     def setUp(self, fn="testdata/twopopssplit_unidirmigrinmidepoch"):
@@ -173,25 +208,44 @@ class TestTwoPopsSplitUniDirMigrInMidEpoch(TestTwoPopsSplit):
 
         scaling = 1.0 / (4.0 * self.pop.N0)
 
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':0,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':1,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-        self.targets.append( {'type':"Coal", 'pop':1, 'epoch':2,
-                              'min':9800,  'max':10200,'truth':10000, 'ess':self.np-1} )
-
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':0,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':1,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':1,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':2,
-                              'min':0.15*scaling, 'max': 0.25*scaling} )
-        self.targets.append( {'type':"Migr", 'from_pop':1, 'to_pop':0, 'epoch':2,
-                              'min':0*scaling, 'max': 0.0001*scaling} )
+        self.targets = [{'type':"Coal", 'pop':0, 'epoch':0,
+                         'min':8653, 'max':10198, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':0,
+                         'min':7897,  'max':9085,'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':0, 'epoch':1,
+                         'min':10057,  'max':10550,  'truth': 10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':1,
+                         'min':9866,  'max':10294,  'truth': 10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':0, 'epoch':2,
+                         'min':9195,  'max':9410, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Coal", 'pop':1, 'epoch':2,
+                         'min':10000,  'max':10000, 'truth':10000, 'ess':self.np-1},
+                         
+                        {'type':"Recomb",
+                         'min':9.88e-9, 'max':1.014e-8, 'truth':1e-8, 'ess':self.np-1},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':8.49e-7},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':4.47e-7},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':2.64e-6, 'max':3.88e-6},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':2.29e-6, 'max':3.33e-6},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':0, 'max':0},
+                         
+                        {'type':"Migr", 'from_pop':0, 'to_pop':1, 'epoch':0,
+                         'min':5e-6, 'max':5e-6} ]
 
         for t in self.targets:
             if t['type'] == "Migr":
