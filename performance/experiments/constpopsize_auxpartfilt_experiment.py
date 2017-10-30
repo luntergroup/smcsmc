f4f020967d43d88089f19e85e6b31bea0119b85b
Gerton Lunter <gerton.lunter@gmail.com>
2017-10-30 17:42:35 +0000

    added script for experiment showing particles and apf required for proper sampling of 8 haplotypes

--- performance/experiments/constpopsize_auxpartfilt_experiment.py
+++ performance/experiments/constpopsize_auxpartfilt_experiment.py
@@ -21,13 +21,14 @@ import itertools
 
 
 # parameters for this experiment
-inference_reps = 5
+inference_reps = 10
 seqlen = 50e6
-particles = [3000, 10000, 30000]
-emiters = 1
+particles = [1000, 3000, 10000, 30000]
+emiters = 10
 lagfactor = 2
 nsam = 8
-chunks = 4    # don't forget to use -C "-P lunter.prjb -q long.qb -pe shmem <chunks>"
+chunks = 12    # don't forget to use -C "-P lunter.prjb -q long.qb -pe shmem <chunks>"
+initial_ne_factor = 0.75
 
 # name of this experiment
 experiment_name = "constpopsize_auxpartfilt"
@@ -39,45 +40,33 @@ experiment_class = test_const_pop_size.TestConstPopSize
 
 
 # define the experiments
-experiment_pars = [{'length':seqlen,             # global
-                    'simseed':0,                 # repetitions with the same data
-                    'infseed':seed,
-                    'numparticles':numparticles, 
-                    'lag':lagfactor,             # global
-                    'nsam':nsam,                 # global
-                    'bias':bias,
-                    'delay':delay,
-                    'apf':apf}
-                   for (seed, numparticles, (bias, delay, apf)) in itertools.product(
-                           range(inference_reps),
-                           particles,
-                           [(0,0,0),    # vanilla
-                            (2,0,0),    # bias 2, no delay
-                            (5,0,0),    # bias 5, no delay
-                            (10,0,0),   # bias 10, no delay
-                            (2,0.1,0),  # bias 2, delay
-                            (5,0.1,0),  # bias 5, delay
-                            (10,0.1,0), # bias 10, delay
-                            (2,0.1,1),  # bias 2, delay, apf 1
-                            (5,0.1,1),  # bias 5, delay, apf 1
-                            (10,0.1,1), # bias 10, delay, apf 1
-                            (2,0.1,2),  # bias 2, delay, apf 2
-                            (5,0.1,2),  # bias 5, delay, apf 2
-                            (10,0.1,2), # bias 10, delay, apf 2
-                            (2,0,1),    # bias 2, apf 1
-                            (5,0,1),    # bias 5, apf 1
-                            (10,0,1),   # bias 10, apf 1
-                            (2,0,2),    # bias 2, apf 2
-                            (5,0,2),    # bias 5, apf 2
-                            (10,0,2),   # bias 10, apf 2
-                            ])]
+experiment_pars_a = [{'length':seqlen,             # global
+                      'simseed':0,                 # repetitions with the same data
+                      'infseed':seed,
+                      'numparticles':numparticles, 
+                      'lag':lagfactor,             # global
+                      'nsam':nsam,                 # global
+                      'bias':bias,
+                      'delay':delay,
+                      'apf':apf}
+                   for (seed, numparticles, (bias, delay, apf)) in itertools.chain(
+                           itertools.product(
+                               range(inference_reps),
+                               particles,
+                               [(1,0,0)]),
+                           itertools.product(
+                               range(inference_reps),
+                               [ particles[-1] ],
+                               [(1,0,2)])
+                           )]
 
 # run an experiment.  keyword parameters must match those in experiment_pars
 def run_experiment( length, simseed, infseed, numparticles, lag, nsam, bias, delay, apf ):
-    label = "P{}_S{}_B{}_D{}_APF{}".format(numparticles,simseed,bias,delay,apf)
+    label = "P{}_N{}_S{}_B{}_D{}_APF{}".format(numparticles,nsam,simseed,bias,delay,apf)
     if experiment_base.have_result( name = experiment_name,
                                     infseed = infseed,
                                     np = numparticles,
+                                    num_samples = nsam,
                                     bias_strengths = ' '.join(map(str,[bias,1])),
                                     int_parameter = int(2 * delay),
                                     aux_part_filt = apf ):
@@ -92,27 +81,29 @@ def run_experiment( length, simseed, infseed, numparticles, lag, nsam, bias, del
     e.pop.seed = (simseed,)
     e.pop.scrmpath = experiment_base.scrmpath
     e.filename_disambiguator = label
-    e.pop.change_points = [0, 0.01, .25, 0.5, 1, 1.5]
-    e.pop.population_sizes = [[1], [1], [1], [1], [1], [1]]
+    e.pop.change_points = [0, 0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.5, 1, 1.5]
+    e.pop.population_sizes = [[1], [1], [1], [1], [1], [1], [1], [1], [1], [1]]
 
     # set inference parameters
     e.seqlen = length
     e.seed = (infseed,)
     e.np = numparticles
     e.lag = lag
-    e.em = emiters-1
+    e.em = emiters
     e.bias_heights = [400]
     e.bias_strengths = [bias, 1]
     e.delay = delay
+    e.delay_type = "coal"
     e.int_parameter = int(2*delay)   # database does not store delay, so store in spare slot
     e.aux_part_filt = apf
     e.smcsmcpath = experiment_base.smcsmcpath
     e.chunks = chunks
     e.popt = None
-    e.smcsmc_change_points = [0, 0.01, 0.25, 0.5, 1, 1.5]
+    e.smcsmc_change_points = [0, 0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.5, 1, 1.5]
     # need to set pop sizes, migr rates and commands explicitly when change pts
     # differ between simulation and inference:
-    e.smcsmc_initial_pop_sizes = [ [1], [1], [1], [1], [1], [1] ]
+    e.smcsmc_initial_pop_sizes = [ [initial_ne_factor * ne]
+                                   for ne in [1,1,1,1,1,1,1,1,1,1] ]
     e.smcsmc_initial_migr_rates = [ [[0]] for cp in e.smcsmc_change_points ]
     e.smcsmc_migration_commands = [ None for cp in e.smcsmc_change_points ]
 
