30a5404fdafb0986ad0fef2daec7dd44bc361112
Gerton Lunter <gerton.lunter@gmail.com>
2018-08-31 17:22:15 +0100

    more cleanup

--- rescomp.config.sh
+++ rescomp.config.sh
@@ -1,16 +1,14 @@
 # note: also edit Makefile.am when compiling on rescomp
 
 # tell configure which gcc to use:
-module purge
+module unload python
+module unload gcc
 module load gcc/5.4.0
+module load python/2.7.10-gcc5.4.0
 
-# optimizations
-#export CXXMARCH="-march nehalem"
-
-# before execution:
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/apps/well/boost/1.59.0-gcc5.4.0-py27/lib
-export PATH=/apps/well/python/2.7.10/bin:$PATH
+#export PATH=/apps/well/python/2.7.10/bin:$PATH
 
-# this is to work around SGE not passing through LD_LIBRARY_PATH
+# work around SGE not passing through LD_LIBRARY_PATH
 export LLP=$LD_LIBRARY_PATH
 
