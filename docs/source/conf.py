6e7edc8f239f282e293b272bcdf926e421e991b2
Christopher Cole <ccole@well.ox.ac.uk>
2019-06-10 18:30:57 +0100

    sort of finish msprime tutorial, have to do a few more things in the python

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../exts'))
 
 
 # -- Project information -----------------------------------------------------
@@ -47,7 +47,8 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'numpydoc'
+    'numpydoc',
+    'hidden_code_block'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
