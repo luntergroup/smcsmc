ea372e7268bfcbd5054c14120ca9e4d42ac48059
Christopher Cole <ccole@well.ox.ac.uk>
2019-06-17 16:45:43 +0100

    Add a conda entry point for the CLI rather than relying on a flat file in the root.

---
+++ smcsmc/cli.py
@@ -0,0 +1,17 @@
+import sys
+import smcsmc
+
+def smcsmc_main():
+    run = smcsmc.Smcsmc(sys.argv[1:])
+    run.print_help_and_exit()
+    run.load_option_file()
+    run.parse_opts()
+    run.validate()
+    run.process_segfiles()
+    run.set_environment()
+    run.define_chunks()
+    run.validate_parameters()
+    run.set_pattern()
+    for em_iter in range(0, run.emiters+1):
+        run.do_iteration(em_iter)
+    run.merge_outfiles()
