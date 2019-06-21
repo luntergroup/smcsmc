88805666bba5f0a2a85565817dc6f78d2e907c7e
Christopher Cole <ccole@well.ox.ac.uk>
2019-06-21 20:54:56 +0100

    Start working on the VCF conversion tool. Not done yet. Should realy be in a devel branch.

--- smcsmc/utils.py
+++ smcsmc/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import glob
 import gzip
+import io
 
 def prune_tree_sequence(tree_sequence_path, num_samples):
     """
@@ -188,72 +189,140 @@ class Expectations:
             counts = self.count_events(path)
             self.update_table(counts)
 
-def vcf_to_seg(vcfs, samples, key, vcfdir="tmp", mask = None, chroms = range(1,23)):
+
+class VcfIter:
+    def __init__(self, filename):
+        """
+        Initiate a VCF class for reading raw genotype data.
+
+        :param str filename: Path to the (optionally GZIPPED) VCF file. 
+                            We support VCF versions above 4.0
+        :return iterator: An iterator over the lines of the VCF in a structured way."""
+        self.file = io.TextIOWrapper(gzip.open(filename, mode = 'rb'))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """
+        Next item in the VCF, return is a tuple where the elements, in order, are:
+
+        0. chrom (str)
+        1. pos (int)
+        2. alleles (list of str)
+        3. geno 1 (int) 
+        4. geno 2 (int)
+        5. phased (bool)
+
+        """
+        line = next(self.file) # Grab the next line of the VCF. Is this safe?
+        while line[0] == "#":
+            line = next(self.file)
+        items = line.strip().split()
+
+        chrom =         items[0]
+        pos =           int(items[1]) 
+        
+        alleles =       [items[3]]
+        for alt_a in items[4].split(","): 
+            alleles.append(alt_a) 
+
+        # There should always be at least one allele
+        assert ( len(alleles) > 0 )
+
+        geno =          items[9][:3] 
+        phased = geno[1] == "|"
+
+        return (chrom, pos, tuple(alleles), (int(geno[0]), int(geno[2])), phased)
+
+
+class MaskIter:
+    def __init__(self, filename):
+        if filename[-3:] == ".gz":
+            self.file = io.TextIOWrapper(gzip.open(filename, "r"))
+        else:
+            self.file = open(filename, "r")
+        self.eof = False
+        self.lastPos = 1
+ 
+        self.readLine()
+
+    def readLine(self):
+        try: 
+            line = next(self.file)
+            fields = line.strip().split()
+            if len(fields) == 2:
+                self.start = int(fields[0])
+                self.end = int(fields[1])
+            else:
+                self.start = int(fields[1]) + 1
+                self.end = int(fields[2])
+        except StopIteration:
+            self.eof = True
+
+    def getVal(self, pos):
+        assert pos >= self.lastPos
+        self.lastPos = pos
+        while pos > self.end and not self.eof:
+            self.readLine()
+        if pos >= self.start and pos <= self.end:
+            return True 
+        else:
+            return False
+
+class MergedMask:
+    """Strictly stolen from Stephen Schiffels"""
+    def __init__(self, mask_iterators):
+        self.maskIterators = mask_iterators
+  
+    def getVal(self, pos):
+        return all((m.getVal(pos) for m in self.maskIterators))
+
+class MergedVcf:
+    def __init__(self, vcf_iterators):
+        self.VcfIterators = vcf_iterators
+
+    def __next__(self):
+        states = [next(vcf) for vcf in self.VcfIterators]
+
+
+
+
+def vcf_to_seg(vcfs, masks, minsize=1000):
     """
     Takes given samples from given VCFs and creates seg files for 
     input to :code:`smcsmc.run_smcsmc()`. 
 
-    .. warning:: 
-        This function is not fully implemented yet and will not work as expected (or at all!)  
-
     Input VCFs do not have to phased, but you should be aware that 
-    if they are not, this will decrease the effectiveness of the lookahead likelihood
-    calculation. 
+    if they are not, this will decrease the effectiveness of the lookahead likelihood.
 
-    Provide a list of VCFs to merge together and a list of sample names 
-    from each to include. These must match up identically. 
-
-    Additionally provide mask files, and an output directory to place
-    the VCFs. By default all somatic chromosomes are anlaysed, but 
-    you can change this by providing an iterator to the chroms
-    argument.
+    Provide a list of VCFs to merge together and masks for each individual in bed format.
 
     :param list vcfs: List of paths to the VCFs which you wish to pull samples from.
-    :param list samples: List of sample names to extract from the VCFs. The order must be the same.
-    :param str key: Name of your output seg files.
-    :param str vcfdir: Serves as a staging ground for intermediate seg files.  
     :param str mask: Directory of similarly named mask files. Currently not implemented.
-    :param iterable chroms: An iterator of the chromosomes you wish to convert. 
-
+   
     :return: Nothing.
     """ 
-    raise NotImplementedError
-    assert (len(vcfs)==len(samples)), "Your lists of samples and VCFs must be the same length." 
-    #for chrom in chroms:
-    #    for vcf, sample in zip(vcfs, samples):
-    #        fname =  f"{vcfdir}/tmp{key}.{sample}.chr{chrom}.vcf.gz"
+    nInd = len(vcfs)
 
-    #        try:
-    #            try_open = gzip.GzipFile( fname, 'r')
-    #            have_files = True
-    #            print(f"You have already created {fname}")
-    #        except:
-    #            have_files = False
+    # Need to progress along the sequence 
+    # Filter out hom ref and indels for simplicity
 
-    #        if not have_files:
-    #            if not os.path.exists(vcfdir):
-    #                os.makedirs(args.vcfdir)
+    vcf_iters = MergedVcf([VcfIter(file) for file in vcfs])
+    mask_iters = MergedMask([MaskIter(file) for file in masks])
 
-    #            fout = gzip.GzipFile (fname, 'w')
-    #            fin = gzip.GzipFile( vcf.format(chrom), 'r')
-    #            print "Reading:\t", vcf.format(chrom)
+    pos = 1
+    last_pos = 1
 
-    #            cols = []
-
-    #            for line in fin:
-    #                elts = line.strip().split('\t')
-    #                if line.startswith('#CHROM'):
-    #                        col = [i for i, e in enumerate(elts) if e == sample]
-    #                        if len(col) == 0:
-    #                            raise ValueError("Could not find individual {}".format(sample))
-    #                        cols.append(col[0])
-    #                if line.startswith('#') and not line.startswith('#CHROM'):
-    #                        fout.write(line)
-    #                else:
-    #                        # filter out hom ref calls, and indel calls
-    #                        if (not elts[cols[0]].startswith("0|0")) and len(elts[3]) == 1 and len(elts[4]) == 1:
-    #                            fout.write('\t'.join(elts[:9] + [elts[cols[0]]] + ["\n"]))
+    for chrom, snp_pos, alelles in vcf_iters:
+        while pos < snp_pos:
+            pos += 1
+            
 
 
 
+    
+    
+    
+    
 
