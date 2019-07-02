d61cf72041fdbc5e01678c49b550425e99e4f101
Christopher Cole <ccole@well.ox.ac.uk>
2019-07-02 16:44:07 +0100

    Tests working now with Nosetest

--- test/test_conversions.py
+++ test/test_conversions.py
@@ -1,22 +1,24 @@
 import smcsmc
 import pdb
+import filecmp
 
 def test_vcf_to_seg():
-    test_vcf = "data/chr{}.vcf.gz"
-    test_mask = "data/mask.chr{}.txt.gz"
+    test_vcf = "data/test.vcf.gz"
+    test_mask = "data/test.bed.gz"
     chr = 2
 
-    sample_1 = "S_Yoruba-1"
-    sample_2 = "S_Mbuti-1"
+    sample_1 = "ID1"
+    sample_2 = "ID2"
 
     smcsmc.vcf_to_seg(
-            [(test_vcf.format(chr), sample_1),
-             (test_vcf.format(chr), sample_2)],
+            [(test_vcf, sample_1),
+             (test_vcf, sample_2)],
             [test_mask, 
              test_mask],
             "out/test.seg.gz",
             tmpdir = "out/tmp",
             key = "testconv",
             chroms = [2]) 
+    filecmp.cmp('out/test.seg.gz', 'data/truth/test_conversion.seg.gz')
 
 test_vcf_to_seg()
