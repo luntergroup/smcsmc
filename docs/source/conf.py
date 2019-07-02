b2a8a3c7f00e9a29f379b15800214a4c48435713
Christopher Cole <ccole@well.ox.ac.uk>
2019-07-02 12:53:14 -0400

    {RTD} path issue?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../exts'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
