ed752ad00c77e84d566beb410cc1d98fa080fba5
Christopher Cole <ccole@well.ox.ac.uk>
2019-10-13 00:11:04 +0100

    Recursion to find paths

--- smcsmc/trees.py
+++ smcsmc/trees.py
@@ -16,8 +16,6 @@ class Event:
         self.pops = [0,0,1,1]
         self.ids = [0,0,1,1]
 
-
-
     def parse_lineages(self, lineages):
         out = [c for c in range(len(lineages)) if lineages[c] == '1']
         if len(out) == 0:
@@ -59,24 +57,27 @@ class Event:
 
         # Have some entries for initialising 
         # internal nodes
-        time1 = 100000
-        time2 = 100000
-        self.NodeTable.add(time1, '0', '0')
-        self.NodeTable.add(time2, '1', '1')
+        time1 = 50000
+        time2 = 50000
+        self.NodeTable.add(time1, 0, 0)
+        self.NodeTable.add(time2, 1, 1)
+        self.NodeTable.add(time1+time2, 0, -1)
 
         self.EdgeTable.add(self.right, self.left, self.nhaps, 0)
-        self.EdgeTable.add(self.right, self.left, self.nhaps, 1)
-        
+        self.EdgeTable.add(self.right, self.left, self.nhaps, 1) 
         self.EdgeTable.add(self.right, self.left, self.nhaps+1, 2)
         self.EdgeTable.add(self.right, self.left, self.nhaps+1, 3)
-        
+        self.EdgeTable.add(self.right, self.left, self.nhaps+2, self.nhaps)
+        self.EdgeTable.add(self.right, self.left, self.nhaps+2, self.nhaps+1)
+
+        self.tables = TableCollection({'Node': self.NodeTable,
+                 'Individual': self.IndividualTable,
+                 'Edge': self.EdgeTable,
+                 'Population': self.PopulationTable})
 
     def printTables(self):   
-        self.IndividualTable.print()
-        self.PopulationTable.print()
-        self.NodeTable.print()
-        self.EdgeTable.print()
-
+        for v in self.tables.dict.values():
+            v.print()
 
 class Migration(Event):
     def __init__(self, tokens):
@@ -123,13 +124,32 @@ class TableCollection:
     def __init__(self, dict):
         self.dict = dict
 
+    def update(self, entry):
+        # 1. Add a coalescent node
+        entry.tables['Node'].add()
+        # 2. Attach the proper nodes to the new one
+        # 3. Find the entries that were cut off (if any)
+        # 4. Destroy their parents 
+        # 5. Check that it still forms a connected tree
+
+    def return_parents(self,node):
+        return(self.parents(node, table = self.dict['Edge'].table))
+
+    def parents(self,node, table): 
+        if int(self.dict['Node'].table.loc[node]['individual']) == -1: 
+            return ["Root"]
+        else: 
+            #p2.extend([int(table.loc[table['child'] == node]['parent'])])
+            new_node = int(table.loc[table['child'] == node]['parent'])
+            return [new_node] + self.parents(new_node,  table = table)
+
 class Table: 
     def __init__(self, columns):
         self.table = pd.DataFrame(columns = columns) 
         self.name = "Generic"
 
     def print(self):
-        print("\n**" + self.name )
+        print("\n*** " + self.name )
         print(self.table) 
 
 
@@ -140,7 +160,9 @@ class NodeTable(Table):
 
     def add(self, time, population, individual):
         row = {'flags': 0, 'time': time, 'population': population, 'individual': individual, 'metadata': None}
-        self.table = self.table.append(pd.DataFrame(row, index = [0]))
+        self.table = self.table.append(pd.DataFrame(row, index = [len(self.table.index)]))
+        self.table.reset_index()
+        #return(len(self.table.index))
 
 
 class IndividualTable(Table):
@@ -156,7 +178,8 @@ class EdgeTable(Table):
 
     def add(self, left, right, parent, child):
         row = {'left': left, 'right': right, 'parent': parent, 'child': child}
-        self.table = self.table.append(pd.DataFrame(row, index = [0]))
+        self.table = self.table.append(pd.DataFrame(row, index = [len(self.table.index)]))
+        #self.table.reset_index()
 
 class PopulationTable(Table):
     def __init__(self):
@@ -166,7 +189,8 @@ class PopulationTable(Table):
 
     def add(self, metadata):
         row = {'metadata': metadata}
-        self.table = self.table.append(pd.DataFrame(row, index = [0]))
+        self.table = self.table.append(pd.DataFrame(row, index = [len(self.table.index)]))
+        #self.table.reset_index()
 
 
 
